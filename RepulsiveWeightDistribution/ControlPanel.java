410e92be66237590c9e932ca279c09e8b2bec8c0
Jack Maguire <jack@med.unc.edu>
2018-07-26 21:26:13 -0400

    packing bars work

--- RepulsiveWeightDistribution/ControlPanel.java
+++ RepulsiveWeightDistribution/ControlPanel.java
@@ -16,6 +16,9 @@ public class ControlPanel extends JPanel implements ActionListener,ChangeListene
     public ControlPanel( View view ){
 	view_ = view;
 
+	lambda_slider_.setValue( 0 );
+	floor_slider_.setValue( 0 );
+
 	setLayout( new GridLayout( 1, 4 ) );
 	add( lambda_slider_ );
 	add( lambda_pair_ );
@@ -42,22 +45,35 @@ public class ControlPanel extends JPanel implements ActionListener,ChangeListene
 	
 	if( e.getSource() == lambda_pair_.button() ){
 	    final double value = Double.parseDouble( lambda_pair_.field().getText() );
-	    System.out.println( value );
+	    lambda_slider_.setValue( weightToSliderValue( value ) );
+	    view_.setLambda( value );
 	}
 
+	else if( e.getSource() == floor_pair_.button() ){
+	    final double value = Double.parseDouble( floor_pair_.field().getText() );
+	    floor_slider_.setValue( weightToSliderValue( value ) );
+	    view_.setFloor( value );
+	}
     }
 
     @Override
     public void stateChanged( ChangeEvent e ){
 	if( e.getSource() == lambda_slider_ ){
 	    final double value = sliderValueToWeight( lambda_slider_.getValue() );
-	    System.out.println( value );
+	    lambda_pair_.field().setText( String.format("%.2f", value ) );
+	    view_.setLambda( value );
+	}
+
+	else if( e.getSource() == floor_slider_ ){
+	    final double value = sliderValueToWeight( floor_slider_.getValue() );
+	    floor_pair_.field().setText( String.format("%.2f", value ) );
+	    view_.setFloor( value );
 	}
     }
 
     private static class TextButtPair extends JPanel {
 
-	private final JTextField field_ = new JTextField( "0.5" );
+	private final JTextField field_ = new JTextField( "0.0" );
 	private final JButton button_ = new JButton( "Set" );
 
 	public TextButtPair(){
--- RepulsiveWeightDistribution/View.java
+++ RepulsiveWeightDistribution/View.java
@@ -3,6 +3,14 @@ import javax.swing.*;
 
 public class View extends JPanel {
 
+    private final static Color packing_color_ = new Color( 10, 10, 100 );
+    private final static Color minimization_color_ = new Color( 100, 10, 10 );
+
+    private double lambda_ = 0.0;
+    private double floor_  = 0.0;
+
+    private final static double[] weights = { 0.02, 0.25, 0.55, 1.0 };
+
     public View(){
 
     }
@@ -10,6 +18,42 @@ public class View extends JPanel {
     public void paint( Graphics g ){
 	g.setColor( Color.BLACK );
 	g.drawLine( 1, 1, 5, 5 );
+
+	final int horiz_unit = getWidth() / 10;
+	final int y_buffer = getHeight() / 4;
+
+	//packing
+	g.setColor( packing_color_ );
+	for( int i=0; i<4; ++i ){
+
+	    final double value = correctedPackingValue( weights[ i ] );
+
+	    final int x = horiz_unit * ( 2 * i + 1 );
+	    final int y = (int) ( y_buffer + (2*y_buffer) * (1-value) );
+	    final int height = (int ) ( (2*y_buffer) * value );
+
+	    g.fillRect( x, y, horiz_unit, height );
+
+	}
+
+    }
+
+    public void setLambda( double setting ){
+	lambda_ = setting;
+	repaint();
+    }
+
+    public void setFloor( double setting ){
+	floor_ = setting;
+	repaint();
+    }
+
+    private double correctedPackingValue( double value ){
+	return floor_ + ( 1 - floor_ ) * value;
+    }
+
+    private double correctedMinimizationValue( double value, double next_value ){
+	return floor_ + ( 1 - floor_ ) * ( lambda_ * next_value + (1 - lambda_) * value );
     }
 
 }
