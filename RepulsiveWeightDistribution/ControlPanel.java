aecd9b9a3884b0c14be70ef18d76734267e1c4a8
Jack Maguire <jack@med.unc.edu>
2018-07-26 20:32:04 -0400

    ControlPanel.java

---
+++ RepulsiveWeightDistribution/ControlPanel.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class ControlPanel extends JPanel {
+
+    private static View view_;
+
+    public ControlPanel( View view ){
+	view_ = view;
+    }
+
+}
