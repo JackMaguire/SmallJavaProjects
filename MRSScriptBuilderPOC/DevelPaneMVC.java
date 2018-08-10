63a769c5b1b39debb6866dc29115998f861c3b9f
Jack Maguire <jack@med.unc.edu>
2018-08-10 15:26:25 -0700

    DevelPaneMVC.java

--- MRSScriptBuilderPOC/DevelPaneMVC.java
+++ MRSScriptBuilderPOC/DevelPaneMVC.java
@@ -3,11 +3,23 @@ import javax.swing.*;
 
 public class DevelPaneMVC implements MyMVC {
 
-    private static DevelPaneModel model_;
+    private static DevelPaneModel model_ = new DevelPaneModel();
+    private String title_;
 
-    public JPanel getNewView(){
+    public DevelPaneMVC( String title ){
+	title_ = title;
+    }
+
+    @Override
+    public String getTitle() {
+	return title_;
+    }
+
+    @Override
+    public JPanel getNewView() {
 	DevelPaneView view = new DevelPaneView( model_ );
 	view.setController( new DevelPaneController( model_, view ) );
+	return view;
     }
 
 }
