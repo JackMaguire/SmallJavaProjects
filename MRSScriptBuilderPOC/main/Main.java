f56379a65d700968970190e4aac00647b8a4d373
Jack Maguire <jack@med.unc.edu>
2018-08-10 16:24:34 -0700

    Got hash map up and running

--- MRSScriptBuilderPOC/main/Main.java
+++ MRSScriptBuilderPOC/main/Main.java
@@ -5,15 +5,22 @@ import javax.swing.*;
 
 import devel_panes.*;
 
+import java.util.HashMap;
+
 public class Main {
 
     private final static DevelPaneMVC  devel_pane1_mvc_ = new DevelPaneMVC( "DevelPane" ); 
     private final static DevelPane2MVC devel_pane2_mvc_ = new DevelPane2MVC(); 
     private final static DevelPane3MVC devel_pane3_mvc_ = new DevelPane3MVC(); 
 
-    private final static MyMVC[] mvc_options = { devel_pane1_mvc_, devel_pane2_mvc_, devel_pane3_mvc_ };
+    //private final static MyMVC[] mvc_options = { devel_pane1_mvc_, devel_pane2_mvc_, devel_pane3_mvc_ };
+    private final static HashMap< String, MyMVC > panes_ = new HashMap< String, MyMVC >();
 
     public static void main( String[] args ){
+	panes_.put( "DevelPane1", devel_pane1_mvc_ );
+	panes_.put( "DevelPane2", devel_pane2_mvc_ );
+	panes_.put( "DevelPane3", devel_pane3_mvc_ );
+
 	JFrame F = new JFrame( "MultistageRosettaScripts" );
 	F.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
 	F.setExtendedState( JFrame.MAXIMIZED_BOTH );
@@ -26,8 +33,8 @@ public class Main {
 
 	public MainView(){
 	    setLayout( new GridLayout( 1, 2 ) );
-	    add( new PanelWrapper( devel_pane1_mvc_.getNewView(), mvc_options ) );
-	    add( new PanelWrapper( devel_pane2_mvc_.getNewView(), mvc_options ) );
+	    add( new PanelWrapper( panes_, "DevelPane1" ) );
+	    add( new PanelWrapper( panes_, "DevelPane2" ) );
 	}
 
     }
--- MRSScriptBuilderPOC/main/PanelWrapper.java
+++ MRSScriptBuilderPOC/main/PanelWrapper.java
@@ -3,26 +3,35 @@ package main;
 import java.awt.*;
 import javax.swing.*;
 
+import java.util.*;
+
 public class PanelWrapper extends JPanel {
 
-    public PanelWrapper( JPanel panel, MyMVC[] mvc_options ){
+    public PanelWrapper( HashMap< String, MyMVC > panes, String key ){
 	setLayout( new BorderLayout() );
-	add( panel, BorderLayout.CENTER );
+	add( panes.get( key ).getNewView(), BorderLayout.CENTER );
 
-	final SouthBar south_bar = new SouthBar( mvc_options );
+	final SouthBar south_bar = new SouthBar( panes );
 	add( south_bar, BorderLayout.SOUTH );
     }
 
     private final static class SouthBar extends JPanel {
-	final MyMVC[] mvc_options_;
+	final private HashMap< String, MyMVC > panes_;
 
-	public SouthBar( MyMVC[] mvc_options ){
-	    mvc_options_ = mvc_options;
+	public SouthBar( HashMap< String, MyMVC > panes ){
+	    panes_ = panes;
+
+	    final JComboBox< String > i = new JComboBox< String >();
+
+	    Iterator< ? > it = panes_.entrySet().iterator();
+	    while ( it.hasNext() ){
+		Map.Entry pair = (Map.Entry) it.next();
+		i.addItem( ""+pair.getKey() );
+	    }
+	    add( i );
 	}
 
-	public void paint( Graphics g ){
-	    super.paint( g );
-
+	public void paintComponent( Graphics g ){
 	    g.setColor( Color.BLACK );
 	    g.fillRect(0,0,getWidth(),getHeight());
 	}
