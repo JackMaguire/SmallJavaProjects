3cc72c9463862d40611161089a1b24d67524dcd8
Jack Maguire <jack@med.unc.edu>
2018-08-10 15:06:30 -0700

    DevelPaneModel.java

---
+++ MRSScriptBuilderPOC/DevelPaneModel.java
@@ -0,0 +1,13 @@
+public class DevelPaneModel {
+
+    private int value_ = 0;
+
+    public void setValue( int setting ){
+	value_ = setting;
+    }
+
+    public void Value(){
+	return value_;
+    }
+
+}
