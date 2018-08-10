91f159b2a1f75e711c190d19041e56bbe20cc05e
Jack Maguire <jack@med.unc.edu>
2018-08-10 15:26:25 -0700

    DevelPaneController.java

---
+++ MRSScriptBuilderPOC/DevelPaneController.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class DevelPaneController implements ChangeListener {
+
+    final private DevelPaneModel model_;
+    final private DevelPaneView view_;
+
+    public DevelPaneController( DevelPaneModel model, DevelPaneView view ){
+	model_ = model;
+	view_ = view;
+    }
+
+    @Override
+    public void stateChanged( ChangeEvent e ){
+	final JSlider slider = (JSlider) e.getSource();
+	final int val = slider.getValue();
+	view_.repaint();
+    }
+
+
+}
