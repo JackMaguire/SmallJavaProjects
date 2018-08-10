b753cee894ba2b4de57c7d786c582fd47ee50d1d
Jack Maguire <jack@med.unc.edu>
2018-08-10 15:26:42 -0700

    DevelPaneView.java

---
+++ MRSScriptBuilderPOC/DevelPaneView.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class DevelPaneView extends JPanel {
+    
+    private final DevelPaneModel model_;
+    private final JSlider slider_ = new JSlider( 0, 255 );
+
+    public DevelPaneView( DevelPaneModel model ){
+	model_ = model;
+	add( slider_ );
+    }
+
+    public void setController( DevelPaneController controller ){
+	slider_.addChangeListener( controller );
+    }
+
+    public void paint( Graphics g ){
+	final int val = model_.getValue();
+	g.setColor( new Color( val, val, val ) );
+	g.fillRect( 0, 0, getWidth(), getHeight() );
+    }
+
+}
