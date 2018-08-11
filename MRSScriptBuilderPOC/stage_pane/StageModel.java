cd4d8de701298d5d22c5c2dd33fa4ca8c4c2cf0c
Jack Maguire <jack@med.unc.edu>
2018-08-10 17:15:55 -0700

    stage_pane/StageModel.java

---
+++ MRSScriptBuilderPOC/stage_pane/StageModel.java
@@ -0,0 +1,15 @@
+package stage_pane;
+
+import java.util.*;
+
+//more of a struct than a class
+public class StageModel {
+    public int num_runs_per_input_struct = 1;
+    public int total_num_results_to_keep = 1;
+    public int result_cutoff = 0;
+    public int max_num_results_to_keep_per_instance = 0;
+    public int max_num_results_to_keep_per_input_struct = 0;
+    public boolean merge_results_after_this_stage = false;
+
+    public ArrayList< MoverOrFilter > protocol;
+}
