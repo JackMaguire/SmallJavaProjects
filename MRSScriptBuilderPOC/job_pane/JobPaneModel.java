01c5b109f2a7e19eef3c6c46e5e3653f11ceb90e
Jack Maguire <jack@med.unc.edu>
2018-08-10 17:02:57 -0700

    JobPaneModel.java

---
+++ MRSScriptBuilderPOC/job_pane/JobPaneModel.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class JobPaneModel {
+
+    private int num_jobs_ = 1;
+
+    private ArrayList< Integer > total_num_jobs_to_keep_per_stage_;
+
+    public JobPaneModel(){
+
+    }
+
+}
