6ef909bbb8b555fb8d1774389ff695d8870d361c
Jack Maguire <jack@med.unc.edu>
2018-08-10 17:42:04 -0700

    Added comment and changed defaults in StageModel.java

--- MRSScriptBuilderPOC/stage_pane/StageModel.java
+++ MRSScriptBuilderPOC/stage_pane/StageModel.java
@@ -4,9 +4,10 @@ import java.util.*;
 
 //more of a struct than a class
 public class StageModel {
+    //TODO double check defaults
     public int num_runs_per_input_struct = 1;
-    public int total_num_results_to_keep = 1;
-    public int result_cutoff = 0;
+    public int total_num_results_to_keep = 100;
+    public int result_cutoff = 1;
     public int max_num_results_to_keep_per_instance = 0;
     public int max_num_results_to_keep_per_input_struct = 0;
     public boolean merge_results_after_this_stage = false;
