772881ca609620f7f47bf9546bf719ce3ee31836
Jack Maguire <jack@med.unc.edu>
2018-08-10 17:15:56 -0700

    stage_pane/StagePaneModel.java

---
+++ MRSScriptBuilderPOC/stage_pane/StagePaneModel.java
@@ -0,0 +1,22 @@
+package stage_pane;
+
+import java.util.*;
+
+public class StagePaneModel {
+
+    //private int num_stages_ = 1;
+    private ArrayList< StageModel > total_num_jobs_to_keep_per_stage_;
+
+    public StagePaneModel(){
+
+    }
+
+    public void addBlankStageAtEnd(){
+	//TODO
+    }
+
+    public void addBlankStageAtPosition(){
+	//TODO
+    }
+
+}
