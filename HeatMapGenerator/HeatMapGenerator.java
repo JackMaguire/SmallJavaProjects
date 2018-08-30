8ec9c9e1cba06a981f8f7a4b6b0bd3fe44b9007d
Jack Maguire <jackmaguire1444@gmail.com>
2018-08-30 15:15:07 -0400

    Added main class

---
+++ HeatMapGenerator/HeatMapGenerator.java
@@ -0,0 +1,18 @@
+import java.io.*;
+public class HeatMapGenerator {
+
+	//Settings:
+	static String filename;
+	
+	public static void main( String[] args ) throws IOException {
+		
+		System.out.println("!");
+		
+	}
+
+	private static void parse_args( String[] args ) {
+		for( int i=0; i<args.length; ++i) {
+			
+		}
+	}
+}
