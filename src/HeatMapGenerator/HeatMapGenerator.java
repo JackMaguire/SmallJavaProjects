81a668c7f76feba08bd98ee76eb0e191ee55666a
Jack Maguire <jackmaguire1444@gmail.com>
2018-08-30 15:25:47 -0400

    parse_args()

--- src/HeatMapGenerator/HeatMapGenerator.java
+++ src/HeatMapGenerator/HeatMapGenerator.java
@@ -3,17 +3,25 @@ import java.io.*;
 public class HeatMapGenerator {
 
 	//Settings:
-	static String filename;
+	public static String filename;
+	public static String output;
 	
 	public static void main( String[] args ) throws IOException {
 		
-		System.out.println("!");
+		parse_args( args );
 		
 	}
 
 	private static void parse_args( String[] args ) {
 		for( int i=0; i<args.length; ++i) {
-			
+			if( args[i].equals( "--filename" ) ) {
+				filename = args[ ++i ];
+				continue;
+			}
+			if( args[i].equals( "--output" ) ) {
+				output = args[ ++i ];
+				continue;
+			}
 		}
 	}
 }
