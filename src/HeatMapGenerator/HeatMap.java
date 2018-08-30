219557bf680718e0cb921fa40a6db900312325b5
Jack Maguire <jackmaguire1444@gmail.com>
2018-08-30 15:39:25 -0400

    Formatting

--- src/HeatMapGenerator/HeatMap.java
+++ src/HeatMapGenerator/HeatMap.java
@@ -5,39 +5,40 @@ public class HeatMap {
 	private final double[][] heat_;
 	private final double[] x_vals_;
 	private final double[] y_vals_;
-	//private final int min
+	// private final int min
 
 	private final double r_squared_;
-	
+
 	private double max_heat_ = 0;
-	
+
 	public HeatMap( double min_x, double max_x, double dx, double min_y, double max_y, double dy, double r ) {
-		r_squared_ = r*r;
-		
-		final int num_x = 1 + (int)( (max_x - min_x)/(dx) );
-		final int num_y = 1 + (int)( (max_y - min_y)/(dy) );
+		r_squared_ = r * r;
+
+		final int num_x = 1 + (int) ( ( max_x - min_x ) / ( dx ) );
+		final int num_y = 1 + (int) ( ( max_y - min_y ) / ( dy ) );
 		heat_ = new double[ num_x ][ num_y ];
-		for( int i=0; i < num_x; ++i ) {
-			for( int j=0; j < num_y; ++j ) {
+		for( int i = 0; i < num_x; ++i ) {
+			for( int j = 0; j < num_y; ++j ) {
 				heat_[ i ][ j ] = 0;
 			}
 		}
-		
+
 		x_vals_ = new double[ num_x ];
-		for( int i=0; i < num_x; ++i ) {
-			x_vals_[ i ] = min_x + (i*dx);
+		for( int i = 0; i < num_x; ++i ) {
+			x_vals_[ i ] = min_x + ( i * dx );
 		}
-		
+
 		y_vals_ = new double[ num_y ];
-		for( int i=0; i < num_y; ++i ) {
-			y_vals_[ i ] = min_y + (i*dy);
+		for( int i = 0; i < num_y; ++i ) {
+			y_vals_[ i ] = min_y + ( i * dy );
 		}
 	}
-	
+
 	public void addVal( double x, double y ) {
-		for( int i=0; i<x_vals_.length; ++i ) {
-			for( int j=0; j<y_vals_.length; ++j ) {
-				final double dist_sq = (x_vals_[i]-x)*(x_vals_[i]-x) + (y_vals_[j]-y)*(y_vals_[j]-y);
+		for( int i = 0; i < x_vals_.length; ++i ) {
+			for( int j = 0; j < y_vals_.length; ++j ) {
+				final double dist_sq = ( x_vals_[ i ] - x ) * ( x_vals_[ i ] - x )
+						+ ( y_vals_[ j ] - y ) * ( y_vals_[ j ] - y );
 				if( dist_sq <= r_squared_ ) {
 					++heat_[ i ][ j ];
 					if( heat_[ i ][ j ] > max_heat_ ) {
@@ -47,10 +48,10 @@ public class HeatMap {
 			}
 		}
 	}
-	
+
 	public void normalize() {
-		for( int i=0; i < x_vals_.length; ++i ) {
-			for( int j=0; j < y_vals_.length; ++j ) {
+		for( int i = 0; i < x_vals_.length; ++i ) {
+			for( int j = 0; j < y_vals_.length; ++j ) {
 				heat_[ i ][ j ] /= max_heat_;
 			}
 		}
--- src/HeatMapGenerator/HeatMapGenerator.java
+++ src/HeatMapGenerator/HeatMapGenerator.java
@@ -1,28 +1,29 @@
 package HeatMapGenerator;
+
 import java.io.*;
+
 public class HeatMapGenerator {
 
-	//Settings:
+	// Settings:
 	public static String filename;
 	public static String output;
-	
+
 	public static void main( String[] args ) throws IOException {
-		
+
 		parse_args( args );
-		
+
 	}
 
 	private static void parse_args( String[] args ) {
-		for( int i=0; i<args.length; ++i) {
-			if( args[i].equals( "--filename" ) ) {
+		for( int i = 0; i < args.length; ++i ) {
+			if( args[ i ].equals( "--filename" ) ) {
 				filename = args[ ++i ];
 				continue;
 			}
-			if( args[i].equals( "--output" ) ) {
+			if( args[ i ].equals( "--output" ) ) {
 				output = args[ ++i ];
 				continue;
 			}
 		}
 	}
 }
-
