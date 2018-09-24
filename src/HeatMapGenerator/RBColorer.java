f86ca07cd2a5977cfe5eb718d5b062a0d19e0d0a
Jack Maguire <jackmaguire1444@gmail.com>
2018-09-24 16:29:14 -0400

    RBColorer

---
+++ src/HeatMapGenerator/RBColorer.java
@@ -0,0 +1,64 @@
+package HeatMapGenerator;
+
+import java.awt.Color;
+
+public class RBColorer implements Colorer {
+
+	public RBColorer() {
+	}
+
+	@Override
+	public Color colorForVal( double val ) {
+		int a = (int) ( 255 * val );
+		if( a < 0 )
+			a = 0;
+		if( a > 255 )
+			a = 255;
+
+		Color c = null;
+		try {
+			c = new Color( 255, 255 - a, 255 - a );
+		}
+		catch( Exception e ) {
+			System.out.println( "val: " + val + " returned a: " + a );
+			System.exit( 1 );
+		}
+		return c;
+	}
+	
+	@Override
+	public Color colorForVal( double val1, double val2 ) {//val1 is red, val2 is blue
+		/*    val1    val2    R   G   B
+		 *       0       1    0   0  255
+		 *       1       0   255  0   0
+		 *       0       0   255 255 255   
+		 *       1       1   255  0  255
+		 */      
+		
+		int a = (int) ( 255 * val1 );
+		if( a < 0 )
+			a = 0;
+		if( a > 255 )
+			a = 255;
+
+		int b = (int) ( 255 * val2 );
+		if( b < 0 )
+			b = 0;
+		if( b > 255 )
+			b = 255;
+		
+		int G = 255 - Math.max( a, b );
+		int R = 255; 
+		if( val1 < val2 ) {
+			R -= ( b - a );
+		}
+		int B = 255;
+		if( val2 < val1 ) {
+			R -= ( a - b );
+		}
+		
+		return new Color( R, G, B );
+
+	}
+
+}
